0d9fa3d04fb30b5dca72c4faa200ecfb2e573cb6
Alay Shah <alaycshah@gmail.com>
2025-11-05 01:23:07 -0500

    small changes, still not great

--- wasm-processor/src/halations/color_tint.rs
+++ wasm-processor/src/halations/color_tint.rs
@@ -35,40 +35,42 @@ fn f32_to_u8(c: f32) -> u8 {
 
 #[wasm_bindgen]
 pub fn lighten_overlay_image(
-    image: &mut [u8],
-    halation_mask: &[u8]
+    base: &mut [u8],
+    overlay: &[u8]
 ){
     let mut pixel = 0;
-    while pixel < image.len() {
- 
+    while pixel < base.len() {
+        let base_a = u8_to_f32(base[pixel+3]);
+        let overlay_a = u8_to_f32(overlay[pixel+3]);
         
-        if halation_mask[pixel+3] > 0 && image[pixel+3] > 0 { 
+        if base_a > 0.0 && overlay_a > 0.0 { 
             // if both pixels are not transparent
-
-            let image_alpha_normalized = u8_to_f32(image[pixel+3]);
-            let mask_alpha_normalized = u8_to_f32(halation_mask[pixel+3]);
-
-            let resulting_alpha_normalized = image_alpha_normalized + mask_alpha_normalized * (1.0 -image_alpha_normalized);
-
-            for i in 0..=2 {
-                let image_color_alpha_normalized = (u8_to_f32(image[pixel+i]))/image_alpha_normalized;
-                let mask_color_alpha_normalized = (u8_to_f32(halation_mask[pixel+i]))/mask_alpha_normalized;
-                let max_color_alpha_normal = image_color_alpha_normalized.max(mask_color_alpha_normalized);
-
-                image[pixel+i] = f32_to_u8((((1.0-mask_alpha_normalized)*image_color_alpha_normalized) + ((1.0-image_alpha_normalized)*mask_color_alpha_normalized) + (image_alpha_normalized * mask_alpha_normalized * max_color_alpha_normal))/resulting_alpha_normalized);
-
-            }
-            image[pixel+3] = f32_to_u8(resulting_alpha_normalized);
             
 
-        } else if halation_mask[pixel+3] > 0 { 
+            let final_a = base_a + overlay_a * (1.0 - base_a);
+
+            for i in 0..=2 {
+                let base_c = u8_to_f32(base[pixel+i]);
+                let overlay_c = u8_to_f32(overlay[pixel+i]);
+                let base_c_a = base_c / base_a;
+                let overlay_c_a = overlay_c / overlay_a;
+                let max_c_a = base_c_a.max(overlay_c_a);
+                let result_c_a = (1. - overlay_a) * base_c + (1. - base_a) * overlay_c + base_a * overlay_a * max_c_a;
+                let final_c = result_c_a / final_a;
+                base[pixel+i] = f32_to_u8(final_c)
+
+            }
+            base[pixel+3] = f32_to_u8(final_a);
+            
+
+        } else if overlay_a > 0.0 && base_a == 0.0 { 
             // if only the halation mask pixel is non zero 
             // and the image pixel alpha is 0 (edge case but might as well handle)
             // just set it as the halation pixel
-            image[pixel] = halation_mask[pixel];
-            image[pixel+1] = halation_mask[pixel+1];
-            image[pixel+2] = halation_mask[pixel+2];
-            image[pixel+3] = halation_mask[pixel+3];
+            base[pixel] = overlay[pixel];
+            base[pixel+1] = overlay[pixel+1];
+            base[pixel+2] = overlay[pixel+2];
+            base[pixel+3] = overlay[pixel+3];
         }
         // other wise the only halation pixel is transparent in which case we dont do anyting
         pixel += 4;
